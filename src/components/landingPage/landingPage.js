d7dd06d58f01b0100fc15b6f05bfb819b2a78377
pphuditt <77424831+pphuditt@users.noreply.github.com>
2022-05-19 15:29:17 +0700

    update top part landing page

--- src/components/landingPage/landingPage.js
+++ src/components/landingPage/landingPage.js
@@ -14,17 +14,51 @@ import DateAdapter from "@mui/lab/AdapterMoment";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
 import Grid from "@mui/material/Grid";
-import Button from '@mui/material/Button';
+import Button from "@mui/material/Button";
 import * as React from "react";
+import { api } from "../../service/api";
+import { useEffect, useState } from "react";
+import authHeader from "../../service/AuthProvider";
+import { useNavigate } from "react-router-dom";
 
-function LandingPage() {
-  const [destination, setDest] = React.useState("");
-  // const dest = [Japan, NewYork, India];
+function LandingPage(props) {
+  const [depart, setDepart] = React.useState("");
+  const [arrive, setArrive] = React.useState("");
+  const [airport, setAirport] = React.useState([]);
   const [date, setDate] = React.useState(moment());
 
-  const handleChange = (event) => {
-    setDest(event.target.destination);
+  const navigate = useNavigate();
+
+  // const {depart, arrive, date} = props;
+
+  
+  const clickEventHandler = () => {
+    if (depart === null || arrive === null) return alert("Please select your destination");
+    else
+    navigate("../availableFlight");
   };
+
+  useEffect(() => {
+    api
+      .get("airport", {
+        headers: authHeader(),
+      })
+      .then((res) => {
+        let tmp1 = res.data;
+        let tmp2 = [];
+        // tmp1 = tmp1.filter((airport) => {
+        //     return airport.code !== "BKK"
+        // });
+        tmp1.map((item) => {
+          tmp2.push(item.code);
+        });
+        setAirport(tmp2);
+      });
+  }, []);
+
+  // const handleChange = (event) => {
+  //   setDest(event.target.destination);
+  // };
 
   return (
     <div className="whole-landing-page">
@@ -46,10 +80,7 @@ function LandingPage() {
           }}
         >
           <Box sx={{ minWidth: 120 }}>
-            <Grid container spacing={2}
-              paddingTop={10}
-              paddingLeft={20}
-            >
+            <Grid container spacing={2} paddingTop={10} paddingLeft={20}>
               <Grid item xs={4}>
                 <FormControl
                   style={{
@@ -57,69 +88,87 @@ function LandingPage() {
                     backgroundColor: "#E8E8E8",
                   }}
                 >
-                  <InputLabel id="departure">Depart</InputLabel>
+                  <InputLabel id="departure">From</InputLabel>
                   <Select
                     labelId="departure-select-label"
                     id="departure"
-                    value={destination}
-                    label="Depart"
-                    onChange={handleChange}
+                    value={depart}
+                    label="From"
+                    onChange={(event) => {
+                      setDepart(event.target.value);
+                      // console.log(event.target.value);
+                    }}
                   >
-                    <MenuItem destination={10}>Thailand</MenuItem>
-                    <MenuItem destination={20}>New York</MenuItem>
-                    <MenuItem destination={30}>India</MenuItem>
+                    {airport.map((to) => {
+                      return <MenuItem value={to}>{to}</MenuItem>;
+                    })}
                   </Select>
                 </FormControl>
               </Grid>
 
               <Grid item xs={4}>
-              <FormControl
-              style={{
-                width: "100%",
-                backgroundColor: "#E8E8E8",
-              }}
-            >
-              <InputLabel id="arrive">Arrive</InputLabel>
-              <Select
-                labelId="arrive-select-label"
-                id="arrive"
-                value={destination}
-                label="Arrive"
-                onChange={handleChange}
-              >
-                <MenuItem destination={10}>Germany</MenuItem>
-                <MenuItem destination={20}>Japan</MenuItem>
-                <MenuItem destination={30}>New York</MenuItem>
-              </Select>
-            </FormControl>
+                <FormControl
+                  style={{
+                    width: "100%",
+                    backgroundColor: "#E8E8E8",
+                  }}
+                >
+                  <InputLabel id="arrive">To</InputLabel>
+                  <Select
+                    labelId="arrive-select-label"
+                    id="arrive"
+                    value={arrive}
+                    label="To"
+                    onChange={(event) => {
+                      setArrive(event.target.value);
+                    }}
+                  >
+                    {airport.map((to) => {
+                      if (depart !== "BKK") return <MenuItem value={to}>{"BKK"}</MenuItem>;
+                      else
+                      return <MenuItem value={to}>{to}</MenuItem>;
+                    })}
+                  </Select>
+                </FormControl>
               </Grid>
 
               <Grid item xs={4}>
-              <LocalizationProvider dateAdapter={DateAdapter} >
-              <DatePicker
-                label="Departure Date"
-                value={date}
-                onChange={(newValue) => {
-                  setDate(newValue);
-                }}
-                renderInput={(params) => <TextField {...params} />}
-              />
-            </LocalizationProvider>
+                <LocalizationProvider dateAdapter={DateAdapter}>
+                  <DatePicker
+                    label="Departure Date"
+                    value={date}
+                    onChange={(newValue) => {
+                      setDate(newValue);
+                    }}
+                    renderInput={(params) => <TextField {...params} />}
+                  />
+                </LocalizationProvider>
               </Grid>
             </Grid>
-            <Grid container spacing={2} paddingLeft={0} paddingRight={0} paddingTop={4} textAlign="center">
+            <Grid
+              container
+              spacing={2}
+              paddingLeft={0}
+              paddingRight={0}
+              paddingTop={4}
+              textAlign="center"
+            >
               <Grid item xs={12}>
-              <Button variant="contained" sx={{
-                width: "24%",
-                height: "64px",
-                backgroundColor: "#F4C300",
-                fontSize: 24,
-                "&:hover": {backgroundColor: "#DEB100"}
-                }}>Find</Button>
+                <Button
+                  variant="contained"
+                  sx={{
+                    width: "24%",
+                    height: "64px",
+                    backgroundColor: "#F4C300",
+                    fontSize: 24,
+                    "&:hover": { backgroundColor: "#DEB100" },
+                  }}
+                  onClick = {clickEventHandler}
+                >
+                  Find
+                </Button>
               </Grid>
             </Grid>
-            
-            
           </Box>
         </Card>
       </div>
